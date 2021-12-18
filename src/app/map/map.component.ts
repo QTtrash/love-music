148a6c32ae1e00051ecb01a4f368cb2f655908d0
Suren Mouchsiadis <smouchsiadis@gmail.com>
2021-12-18 19:28:13 +0100

    create marker for the user on the map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MapService } from './map.service';
 import { Coordinates } from './map.service';
-import { latLng, tileLayer } from 'leaflet';
+import { icon, latLng, marker, polyline, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'map-component',
@@ -10,23 +10,33 @@ import { latLng, tileLayer } from 'leaflet';
   providers: [MapService],
 })
 export class MapComponent implements OnInit {
-  options = {
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors',
-      }),
-    ],
-    zoom: 7,
-    center: latLng([46.879966, -121.726909]),
-  };
-
   loaded = false;
   locationAccess = false;
   latitude = 0;
   longitude = 0;
 
-  source = '';
+  // Define our base layers so we can reference them multiple times
+  streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  });
 
+  user = marker([this.latitude, this.longitude], {
+    icon: icon({
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
+      iconUrl: 'leaflet/marker-icon.png',
+      shadowUrl: 'leaflet/marker-shadow.png',
+    }),
+  });
+
+  // Set the initial set of displayed layers (we could also use the leafletLayers input binding for this)
+  options = {
+    layers: [this.streetMaps, this.user],
+    zoom: 20,
+    center: latLng([this.latitude, this.longitude]),
+  };
   constructor(private mapService: MapService) {}
 
   successCallback({ longitude, latitude }: Coordinates) {
@@ -34,11 +44,11 @@ export class MapComponent implements OnInit {
     this.longitude = longitude;
     this.loaded = true;
     this.locationAccess = true;
-    this.source = `https://www.openstreetmap.org/export/embed.html?bbox=11.645553410053253%2C48.192565960755516%2C11.648745238780975%2C48.19387836444258&amp;layer=mapnik&amp;marker=${this.latitude}%2C${this.longitude}`;
+    this.options.center = latLng([this.latitude, this.longitude]);
+    this.user.setLatLng([this.latitude, this.longitude]);
   }
 
   errorCallback() {
-    console.log('error');
     this.loaded = true;
     this.locationAccess = false;
   }
